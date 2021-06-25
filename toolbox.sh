b0071ed5bc4583f8acb227f13df2b245b7322fe8
KasparPeterson <kasparpeterson@hotmail.com>
2021-06-25 19:21:11 +0300

    Adds code coverage extra bash options.

--- toolbox.sh
+++ toolbox.sh
@@ -9,4 +9,11 @@ function test() {
 
 function coverage() {
   truffle run coverage
+  sleep 3
+  rm coverage.json # CLI option does not seem to exist from --help output
+}
+
+function coverage-html() {
+  coverage
+  open coverage/index.html
 }
